03e635352fea2e8a804984798bf20c575efef231
brojass <brojas61@gmail.com>
2020-02-17 17:06:29 -0300

    def search_instrument and def create_dict_for_search_duplicates working

--- detector.py
+++ detector.py
@@ -10,6 +10,57 @@ class ConfigurationError(Exception):
     pass
 
 
+def create_dict_for_search_duplicates(instruments_list):
+    """
+
+    :param instruments_list:
+    :type instruments_list: list
+    :return:
+    :rtype: dict
+    """
+    instrument_dict = {}
+
+    for line_instrument in instruments_list:
+
+        aux_split_list = []
+        line_instrument_split = line_instrument.split()
+        key = line_instrument_split[0]
+        del line_instrument_split[0:2]
+
+        for x in range(0, 8, 3):
+            aux_split_list.append(line_instrument_split[x])
+
+        instrument_dict[key] = aux_split_list
+
+    return instrument_dict
+
+
+def search_instrument(instruments_list, instruments_ports_list):
+    """
+
+    :param instruments_list:
+    :type instruments_list: list
+    :param instruments_ports_list:
+    :type instruments_ports_list: list
+    :return:
+    :rtype: tuple
+    """
+    finded_list = []
+    not_finded_list = []
+    for instrument_port in instruments_ports_list:
+        for instrument in instruments_list:
+            instrument_split = instrument.split()
+            if 'gcal2' + instrument_port == instrument_split[0]:
+                finded_list.append('gcal2' + instrument_port)
+                # print('match', 'gcal2' + instrument_port)
+
+        if 'gcal2' + instrument_port not in finded_list:
+            not_finded_list.append('gcal2' + instrument_port)
+            # print("don't match", 'gcal2' + instrument_port)
+
+    return finded_list, not_finded_list
+
+
 def instrument_to_search(ports_list):
     """
 
@@ -21,9 +72,15 @@ def instrument_to_search(ports_list):
     instrument_to_search_list = []
     for port_line in ports_list:
         port_line_list = port_line.split()
-        number_port = (port_line_list[1])[-1]
-        instrument_name = port_line_list[3].replace('"', '').lower()
-        instrument_to_search_list.append(instrument_name + number_port)
+        number_port = (port_line_list[1])[-1].strip()
+        instrument_name = port_line_list[3].replace('"', '').lower().strip()
+
+        if not instrument_name + number_port == 'gcal2':
+            instrument_to_search_list.append(instrument_name + number_port)
+
+    if 'canopus4' not in instrument_to_search_list:
+        print("CANOPUS it's not in port 4")
+
     return instrument_to_search_list
 
 
@@ -107,6 +164,9 @@ if __name__ == '__main__':
     port_list = []
     instrument_port_list = []
     instrument_list = []
+    instruments_finded = []
+    instruments_not_finded = []
+
     try:
         port_list = read_configuration('agSeqPorts.pv')
         instrument_list = read_configuration('ag_sf.lut')
@@ -118,11 +178,9 @@ if __name__ == '__main__':
         exit(0)
 
     instrument_port_list = instrument_to_search(port_list)
+    search_instrument(instrument_list, instrument_port_list)
+
+    create_dict_for_search_duplicates(instrument_list)
 
     print_list(port_list)
     print_list(instrument_list)
-
-
-
-
-
