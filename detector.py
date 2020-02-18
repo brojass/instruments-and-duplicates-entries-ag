926117ef743451ed791e3b04fd932c7633f95ed4
brojass <brojas61@gmail.com>
2020-02-18 10:23:16 -0300

    def search_duplicates working and def search_instruments modified

--- detector.py
+++ detector.py
@@ -10,6 +10,26 @@ class ConfigurationError(Exception):
     pass
 
 
+def search_duplicates(dict_tables):
+    """
+
+    :param dict_tables:
+    :type dict_tables: dict
+    :return:
+    :rtype:
+    """
+    compare_dict = {}
+
+    for key, value in dict_tables.items():
+        compare_dict.setdefault(tuple(value), set()).add(key)
+
+    result = [values for key, values in compare_dict.items()
+              if len(values) > 1]
+
+    # print("duplicates", result)
+    return result
+
+
 def create_dict_for_search_duplicates(instruments_list):
     """
 
@@ -22,15 +42,15 @@ def create_dict_for_search_duplicates(instruments_list):
 
     for line_instrument in instruments_list:
 
-        aux_split_list = []
+        value_list = []
         line_instrument_split = line_instrument.split()
         key = line_instrument_split[0]
         del line_instrument_split[0:2]
 
         for x in range(0, 8, 3):
-            aux_split_list.append(line_instrument_split[x])
+            value_list.append(line_instrument_split[x])
 
-        instrument_dict[key] = aux_split_list
+        instrument_dict[key] = value_list
 
     return instrument_dict
 
@@ -43,22 +63,44 @@ def search_instrument(instruments_list, instruments_ports_list):
     :param instruments_ports_list:
     :type instruments_ports_list: list
     :return:
-    :rtype: tuple
+    :rtype: dict
     """
+    finded_dict = {}
+    key = ''
+    value = ''
+
+    finded_gcal2_list = []
     finded_list = []
+    not_finded__gcal2_list = []
     not_finded_list = []
     for instrument_port in instruments_ports_list:
         for instrument in instruments_list:
             instrument_split = instrument.split()
+
             if 'gcal2' + instrument_port == instrument_split[0]:
-                finded_list.append('gcal2' + instrument_port)
+                # finded_gcal2_list.append('gcal2' + instrument_port)
                 # print('match', 'gcal2' + instrument_port)
+                value = 'gcal2' + instrument_port
+            elif instrument_port == instrument_split[0]:
+                # finded_list.append(instrument_port)
+                key = instrument_port
 
-        if 'gcal2' + instrument_port not in finded_list:
-            not_finded_list.append('gcal2' + instrument_port)
-            # print("don't match", 'gcal2' + instrument_port)
+        if not key:
+            finded_dict['emptyKey_' + instrument_port] = value
+        elif not value:
+            finded_dict[key] = 'emptyValue_' + instrument_port
+        else:
+            finded_dict[key] = value
 
-    return finded_list, not_finded_list
+        # if 'gcal2' + instrument_port not in finded_gcal2_list:
+        #     not_finded__gcal2_list.append('gcal2' + instrument_port)
+        #     # print("don't match", 'gcal2' + instrument_port)
+        # elif instrument_port not in finded_list:
+        #     not_finded_list.append(instrument_port)
+
+    print(finded_dict)
+    # print(finded_list, finded_gcal2_list, not_finded_list, not_finded__gcal2_list)
+    return finded_dict
 
 
 def instrument_to_search(ports_list):
@@ -80,15 +122,15 @@ def instrument_to_search(ports_list):
 
     if 'canopus4' not in instrument_to_search_list:
         print("CANOPUS it's not in port 4")
+    else:
+        instrument_to_search_list.remove('canopus4')
 
     return instrument_to_search_list
 
 
 def read_configuration(file_name):
     """
-    This function read the file configuration and have an state machine that separate
-    the host and root folder from the folders who has the files to analyze, and as result
-    have all files found inside the configuration file.
+
     :param file_name: The name of the path file who has the configuration.
     :type file_name: str
     :return: A list with files found inside the configuration file.
@@ -164,8 +206,11 @@ if __name__ == '__main__':
     port_list = []
     instrument_port_list = []
     instrument_list = []
-    instruments_finded = []
-    instruments_not_finded = []
+
+    instruments_finded = {}
+
+    tables_dict = {}
+    duplicate_entries = []
 
     try:
         port_list = read_configuration('agSeqPorts.pv')
@@ -178,9 +223,10 @@ if __name__ == '__main__':
         exit(0)
 
     instrument_port_list = instrument_to_search(port_list)
-    search_instrument(instrument_list, instrument_port_list)
+    instruments_finded = search_instrument(instrument_list, instrument_port_list)
 
-    create_dict_for_search_duplicates(instrument_list)
+    tables_dict = create_dict_for_search_duplicates(instrument_list)
+    duplicate_entries = search_duplicates(tables_dict)
 
     print_list(port_list)
     print_list(instrument_list)
