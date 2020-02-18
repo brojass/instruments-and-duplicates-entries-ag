3df94b91e409b04ce30245619533ecc4ac64d78f
brojass <brojas61@gmail.com>
2020-02-18 18:23:01 -0300

    problem where it's an error in canopus but all it's working

--- detector.py
+++ detector.py
@@ -86,18 +86,21 @@ def search_instrument(instruments_list, instruments_ports_list):
         else:
             finded_dict[key] = value
 
-    print(finded_dict)
+    # print(finded_dict)
     return finded_dict
 
 
 def instrument_to_search(ports_list):
     """
-
-    :param ports_list:
+    Function that separate the number of each port and merge the name of the instrument with it. Also return an
+    string in case that CANOPUS aren't in port 4.
+    :param ports_list: List with the lines that contain the ports and the instrument associated to it.
     :type ports_list: list
-    :return:
-    :rtype: list
+    :return: The name of each instrument and the port where is it. Also return an string in case that
+    CANOPUS aren't in port 4.
+    :rtype: tuple
     """
+    canopus_error = ''
     instrument_to_search_list = []
     for port_line in ports_list:
         port_line_list = port_line.split()
@@ -105,28 +108,25 @@ def instrument_to_search(ports_list):
         instrument_name = port_line_list[3].replace('"', '').lower().strip()
 
         if not instrument_name + number_port == 'gcal2':
-            instrument_to_search_list.append(instrument_name + number_port)
+            if not number_port == '4':
+                instrument_to_search_list.append(instrument_name + number_port)
+            elif not instrument_name + number_port == 'canopus4':
+                canopus_error = "CANOPUS it's not in port 4, instead it's '" + instrument_name + "'"
 
-    if 'canopus4' not in instrument_to_search_list:
-        print("CANOPUS it's not in port 4")
-    else:
-        instrument_to_search_list.remove('canopus4')
-
-    return instrument_to_search_list
+    return instrument_to_search_list, canopus_error
 
 
 def read_configuration(file_name):
     """
-
+    This function read the file that have the actual instruments that are already functioning on differents ports and
+    read the file who has the lock up tables.
     :param file_name: The name of the path file who has the configuration.
     :type file_name: str
     :return: A list with files found inside the configuration file.
     :rtype: list
     :raises: ConfigurationError
     """
-
     found_file_list = []
-
     f = open(file_name, 'r')
 
     for line in f:
@@ -179,9 +179,11 @@ def get_arguments(argv):
     return parser.parse_args(argv[1:])
 
 
-def configure_email(instrument, duplicates):
+def configure_email(instrument, duplicates, canopus_error):
     """
 
+    :param canopus_error:
+    :type canopus_error: str
     :param instrument:
     :type instrument: dict
     :param duplicates:
@@ -189,6 +191,8 @@ def configure_email(instrument, duplicates):
     """
     duplicates_problems = ''
     instruments_problems = ''
+    canopus_problems = canopus_error
+    state = 0
 
     for key, value in instrument.items():
         emptykey = ''
@@ -196,37 +200,89 @@ def configure_email(instrument, duplicates):
         if re.search('emptyKey_', key) or not value:
             if re.search('emptyKey_', key):
                 emptykey_split = key.split('_')
-                emptykey = emptykey_split[-1] + ' could not be found'
+                emptykey = emptykey_split[-1] + ' could not be found on the table' + '\n'
                 if not value:
-                    emptyvalue = ' and gcal2' + emptykey_split[-1] + ' neither'
+                    emptyvalue = 'gcal2' + emptykey_split[-1] + ' could not be found on the table'
             elif not value:
-                emptyvalue = 'gcal2' + key + ' could not be found'
-            instruments_problems = instruments_problems + emptykey + emptyvalue + '\n'
+                emptyvalue = 'gcal2' + key + ' could not be found on the table'
+            instruments_problems = instruments_problems + emptykey + emptyvalue
 
-    print(instruments_problems)
+    # print(instruments_problems)
 
     for key, value in duplicates.items():
         if len(value) > 1:
-            duplicates_problems = duplicates_problems + 'Elements ' + str(value) + ' has duplicate values: ' + str(
+            duplicates_problems = duplicates_problems + 'Entries ' + str(value) + ' has duplicate values: ' + str(
                 key) + '\n'
-    print(duplicates_problems)
+    # print(duplicates_problems)
+
+    duplicates_problems = ''
 
     if instruments_problems and duplicates_problems:
 
         head_instruments = 'Instruments not found:' + "\n" + instruments_problems
         head_duplicates = 'Duplicates issues:' + "\n" + duplicates_problems
-        content = head_instruments + "\n" + head_duplicates
+        print('---------------------------------------------------------------------------------')
+        print('Issues detected in ag_sf.lut')
+        print('---------------------------------------------------------------------------------')
+        if canopus_problems:
+            head_canopus = 'Warning in port 4:' + "\n" + canopus_problems + "\n"
+            print(head_canopus)
+            content = head_canopus + "\n" + head_instruments + "\n" + head_duplicates
+        else:
+            content = head_instruments + "\n" + head_duplicates
+        print(head_instruments)
+        print(head_duplicates)
+        print('\n' + '\n' + 'NOTE: Please check each port with the instrument in agSeqPorts.pv')
+        print('---------------------------------------------------------------------------------')
+        state = 1
         send_email(content)
 
     elif instruments_problems and not duplicates_problems:
 
         head_instruments = 'Instruments not found:' + "\n" + instruments_problems
-        send_email(head_instruments)
+        print('---------------------------------------------------------------------------------')
+        print('Issues detected in ag_sf.lut')
+        print('---------------------------------------------------------------------------------')
+        if canopus_problems:
+            head_canopus = 'Warning in port 4:' + "\n" + canopus_problems + "\n"
+            print(head_canopus)
+            content = head_canopus + "\n" + head_instruments
+        else:
+            content = head_instruments
+        print(content)
+        print('\n' + '\n' + 'NOTE: Please check each port with the instrument in agSeqPorts.pv')
+        print('---------------------------------------------------------------------------------')
+        state = 1
+        send_email(content)
 
     elif not instruments_problems and duplicates_problems:
 
         head_duplicates = 'Duplicates issues:' + "\n" + duplicates_problems
+        print('---------------------------------------------------------------------------------')
+        print('Issues detected in ag_sf.lut')
+        print('---------------------------------------------------------------------------------')
+        if canopus_problems:
+            head_canopus = 'Warning in port 4:' + "\n" + canopus_problems + "\n"
+            print(head_canopus)
+            content = head_canopus + "\n" + head_duplicates
+        else:
+            content = head_duplicates
+        print(content)
+        print('\n' + '\n' + 'NOTE: Please check each port with the instrument in agSeqPorts.pv')
+        print('---------------------------------------------------------------------------------')
+        state = 1
         send_email(head_duplicates)
+
+    # elif canopus_problems and state == 0:
+    #
+    #     print('---------------------------------------------------------------------------------')
+    #     print('Issues detected in ag_sf.lut')
+    #     print('---------------------------------------------------------------------------------')
+    #     head_canopus = 'Warning in port 4:' + "\n" + canopus_problems + "\n"
+    #     print(head_canopus)
+    #     print('\n' + '\n' + 'NOTE: Please check each port with the instrument in agSeqPorts.pv')
+    #     print('---------------------------------------------------------------------------------')
+    #     send_email(head_canopus)
 
 
 def send_email(content):
@@ -259,16 +315,17 @@ if __name__ == '__main__':
     #     print('no configuration files specified')
     #     exit(0)
 
-    # Read configuration file(s)
     port_list = []
-    instrument_port_list = []
+    error_canopus = ''
     instrument_list = []
 
+    instrument_port_list = []
     instruments_finded = {}
 
     tables_dict = {}
     duplicate_entries = []
 
+    # Read configuration file(s)
     try:
         port_list = read_configuration('agSeqPorts.pv')
         instrument_list = read_configuration('ag_sf.lut')
@@ -279,13 +336,13 @@ if __name__ == '__main__':
         print(e)
         exit(0)
 
-    instrument_port_list = instrument_to_search(port_list)
+    # print_list(port_list)
+    # print_list(instrument_list)
+
+    instrument_port_list, error_canopus = instrument_to_search(port_list)
     instruments_finded = search_instrument(instrument_list, instrument_port_list)
 
     tables_dict = create_dict_for_search_duplicates(instrument_list)
     duplicate_entries = search_duplicates(tables_dict)
 
-    configure_email(instruments_finded, duplicate_entries)
-
-    print_list(port_list)
-    print_list(instrument_list)
+    configure_email(instruments_finded, duplicate_entries, error_canopus)
